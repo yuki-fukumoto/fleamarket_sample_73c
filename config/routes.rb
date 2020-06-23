8a38c9facfe6e2c880e8026d10ef4ad848e7c6e5
yuki-fukumoto <yufuyufuyufu@gmail.com>
2020-06-23 22:25:55 +0900

    top_page_main

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'top_page/index'
-  root 'items#index'
+  root 'top_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
