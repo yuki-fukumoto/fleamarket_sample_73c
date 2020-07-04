2e68725f497d4910057892371fabd0f08687ee9e
塩見聡太朗 <shiomisatoshitarou@shiomisatoshitarounoMacBook-Pro.local>
2020-07-04 20:34:53 +0900

    修正完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
-  get "/" => "items#index"
+  root 'items#index'
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
