c4ce1cbf5902a98265f8e1453e9aef28faf154a6
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-07-07 21:45:10 +0900

    購入確認ページ
    what
    購入確認ページのビュー作成
    why
    間違えて購入を防ぐため

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only: [:new] 
+
+  resources :purchases do
+    collection do
+      get :confirm
+      # post :confirm ←あとでこっちにする
+    end
+  end
 end
