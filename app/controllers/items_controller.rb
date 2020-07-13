701559585ac3b3d48d8d7b976d1c8fa94f798468
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-07-13 15:33:43 +0900

    detail_view
    what
    一覧から商品をクリックすると詳細ページに移るようにした
    
    why
    小さい画面では詳細な情報を書くのが難しいから

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    # @items = Item.all
+  end
+
+  def new
+    # @item = Item.new
+  end
+
+  def create
+    # Item.create(item_params)
+    # redirect_to root_path
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top_page#index'
-  resources :items, expect: :index
+  # resources :items, expect: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items, only: [:new] 
+  resources :items 
 
   resources :purchases do
     collection do
