46ab615d098ef44e4fbe30534d42ba5db14bc06c
skipman-77 <h.morikawa5201@gmail.com>
2020-07-14 17:14:18 +0900

    user登録0714

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   validates :nickname, :firstname, :lastname, :firstname_read, :lastname_read, :birthday, :password, presence: {message: "が空欄です"}
-  validates :firstname, :lastname, :firstname_read, :lastname_read, format: { with: /\p{Hiragana}/, message: "は全角で入力してください"}
+  validates :firstname, :lastname, :firstname_read, :lastname_read, format: { with: /\A[一-龥ぁ-ん]/, message: "は全角で入力してください"}
   validates :password, length: { minimum:7, message: "は7文字以上で入力してください"}, confirmation: {message: "の値が一致しません"}
-
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
+  
+  
+
 
   has_many :addresses, dependent: :destroy
   accepts_nested_attributes_for :addresses
@@ -15,12 +18,5 @@ class User < ApplicationRecord
   has_many :purchases, dependent: :destroy
 
 
-  def self.save_from_yaml!(yaml_path)
-    yaml = YAML.load(yaml_path)
-    user = self.new
-    user.name = yaml['name']
-    user.email = yaml['email']
   
-    user.save!
-  end
 end
