10c39ce08ef4027f3826103dd1575de0747f182b
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-07-17 14:20:29 +0900

    未入力項目のチェック
    what
    未入力項目がある場合出品ボタンを押せないようにした(現状一部エラーあり)
    why
    必須項目の記述もれを防ぐため

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -35,3 +35,54 @@ $(document).on("turbolinks:load", function () {
     });
   });
 });
+
+$(function(){
+  $('#submit').on('click', function(){
+    // if($('.image_area').val() === ''){
+    //   alert('画像を1枚以上入力してください！');
+    //   $(this).focus();
+    //   return false;
+    // }
+    if($('.titlename').val() === ''){
+      alert('商品名を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('.explanation').val() === ''){
+      alert('説明を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    // if($('.category_0').val() === ''){
+    //   alert('カテゴリーを選択してください！');
+    //   $(this).focus();
+    //   return false;
+    // }
+    if($('.condition').val() === ''){
+      alert('コンディションを選択してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('.shipping_pay').val() === ''){
+      alert('配送料の負担を選択してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('.shipping_area').val() === ''){
+      alert('発送元の地域を選択してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('.shipping_period').val() === ''){
+      alert('発送までの日数を選択してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('.price').val() === ''){
+      alert('販売価格を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    alert('出品完了！');
+  });
+});
