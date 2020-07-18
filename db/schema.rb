21b52b5b141ba168bc264fac9ac8a4d320fd5f3d
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-07-18 16:45:23 +0900

    drop_image_table
    ## what
    - imageテーブルの削除
    
    ## why
    - 不要なテーブルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_030603) do
+ActiveRecord::Schema.define(version: 2020_07_18_074328) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "firstname", null: false
@@ -50,14 +50,6 @@ ActiveRecord::Schema.define(version: 2020_07_18_030603) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_creditcards_on_user_id"
-  end
-
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image", null: false
-    t.index ["item_id"], name: "index_images_on_item_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -113,7 +105,6 @@ ActiveRecord::Schema.define(version: 2020_07_18_030603) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "creditcards", "users"
-  add_foreign_key "images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
