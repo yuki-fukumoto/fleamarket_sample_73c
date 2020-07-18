035db1ac0df5f490b30f7d2caa38c029b5a405f5
yuki-fukumoto <yufuyufuyufu@gmail.com>
2020-07-18 19:02:07 +0900

    update-category2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -35,7 +35,43 @@ class CategoriesController < ApplicationController
   private
 
   def set_category
-  #パラメーターに入ってくカテゴリーidを基にクリックされたカテゴリーのレコードを取得
-    @category = Category.find(params[:id])
-  end
+    before_action :set_category, only: [:parent, :child, :grandchild]
+
+    def parent
+      #親要素の子要素代入
+      children = @category.children
+      grandchildren = []
+      # ancestryに(親要素id/子要素id)が入っている要素全て取得
+      children.each do |child|
+        grandchildren << Category.where(ancestry: "#{@category.id}/#{child.id}")
+      end
+      @items = []
+      # grandchildrenは（１子要素の孫要素、２子要素の孫要素）という多重配列なのでeach文を二回重ねることで親要素の全ての孫要素のidをwhereメソッドに渡しています。
+      grandchildren.each do |grandchild|
+        grandchild.each do |id|
+          @items += Item.where(category_id: id)
+        end
+      end
+    end
+  
+    def child
+      #子要素の孫要素代入
+      grandchildren = @category.children
+      @items = []
+      #子要素の孫要素という一つの配列なのでeach文は一度で孫要素のid取得
+      grandchildren.each do |grandchild|
+        @items += Item.where(category_id: grandchild.id)
+      end
+    end
+  
+    def grandchild
+      @items = Item.where(category_id: params[:id])
+    end
+  
+    private
+  
+    def set_category
+    #パラメーターに入ってくカテゴリーidを基にクリックされたカテゴリーのレコードを取得
+      @category = Category.find(params[:id])
+    end
 end
