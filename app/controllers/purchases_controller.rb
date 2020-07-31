07dc9178f0a3838fd34a8f69e84cbbd6e52ef7b3
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-07-31 17:22:00 +0900

    add_function_purchase
    ## what
    - purchaseコントローラにpurchaseレコードを生成する記述を追加
    - purchaseモデルにcreate_purchaseメソッドを追加
    - purchaseテーブルにpayjpのchargeレコードのidを登録するためのカラムを追加
    
    ## why
    - 商品購入機能の実装
    →残タスク：商品購入機能のエラーハンドリング、クレカ情報編集機能、削除機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,26 @@
 class PurchasesController < ApplicationController
   def create
+    @item = Item.find(params[:item_id])
+    if @purchase = Purchase.create_charge(purchase_params, @item)
+      @item[:status] = "sold"
+      @item.save
+      redirect_to root_path, notice: "購入しました"
+    else
+      flash.now[:notice] = "申し訳ございません、購入に失敗しました。再度、ご購入手続きお願いいたします"
+      render confirm_purchases_path
+    end
   end
 
   def confirm
+    @purchase = Purchase.new
     @item = Item.find(params[:id])
+    user = User.find(current_user.id)
+    @address = user.addresses.first
+    @creditcard = user.creditcards.first
   end
 
   private
   def purchase_params
+    params.permit(:address_id, :item_id, :creditcard_id).merge(user_id: current_user.id)
   end
 end
