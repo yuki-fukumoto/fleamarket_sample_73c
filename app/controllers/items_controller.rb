6ea7bc71667ed7298c022ea9337bd5cb77493fea
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-08-05 15:42:05 +0900

    編集機能
    what
    元のカテゴリーを変更できるようにした。
    preview.jsで書いたエラーメッセージの記述をalert.jsに移動
    出品、編集ページのビューを微調整
    コンフリクト解消
    why
    カテゴリーを変更しようとしても選択肢が現れなかったから。
    可読性向上のため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :confirm_user_signed_in?, except: [:index, :show]
-  before_action :set_item
 
   def index
     @items = Item.on_sell.includes([:images]).order(created_at: :desc)
@@ -63,18 +62,6 @@ class ItemsController < ApplicationController
   def collection_child_categories
     @categories = Category.get_categories(params[:selected_id])
     render json: @categories
-  end
-
-  # def get_category_children
-  #   @category_children = Category.find(params[:category_id]).children
-  # end
-
-  # def get_category_grandchildren
-  #   @category_grandchildren = Category.find("#{params[:child_id]}").children
-  # end
-  
-  def set_item
-    @item = Item.find(params[:id])
   end
 
   private
