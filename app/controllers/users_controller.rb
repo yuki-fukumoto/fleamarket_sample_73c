adaf706f3d1c02e958c97dc56c9e2d58d28172c8
skipman-77 <h.morikawa5201@gmail.com>
2020-08-08 16:24:14 +0900

    商品購入確認ページ　配送先の登録
    #WHAT
    商品購入確認ページの配送先をユーザー登録情報から引っ張ってこれるようにした
    (複数登録時は無理)
    #WHY
    ダミー情報だった為

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
   end
+
+  def change
+    create_table :adresses do |t|
+      t.references :adresses, index: true, foreign_key: true,
+    create_table :creditcards do |t|
+      t.references :creditcards, index: true, foreign_key: true,
+  
+
+    end
+  end
 end
