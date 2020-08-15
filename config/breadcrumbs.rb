c65f6e1028c4374bd141d790e2e92705d7a2fde1
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-08-15 14:34:05 +0900

    add_bread_crumb_function
    ## what
    - gem gretel導入
    - gretelジェムのコンフィグファイルを作成(config/breadcrumb.rb)
    - 部分テンプレート breadcrumb.html.haml作成
    - 各ページに上記テンプレートを呼び出す記述を追加
    
    ## why
    - 各ページにパンくずナビゲーションを追加
    ⇨UI向上

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,72 @@
+crumb :root do
+  link "トップページ", root_path
+end
+
+crumb :sign_up do
+  link "ユーザー登録", new_user_registration_path
+end
+
+crumb :sign_in do
+  link "ログイン", new_user_session_path
+end
+
+crumb :user_show do
+  link "マイページ", user_path(current_user)
+end
+
+crumb :creditcard_new do
+  link "クレジットカード登録", new_creditcard_path
+  parent :user_show
+end
+
+crumb :creditcard_show do
+  link "クレジットカード情報", creditcard_path
+  parent :user_show
+end
+
+crumb :item_search do
+  link "検索結果一覧", search_items_path
+end
+
+crumb :item_show do
+  link "商品詳細", item_path(params[:id])
+end
+
+crumb :purchase_confirm do
+  link "購入確認画面", confirm_purchase_path(params[:id])
+  parent :item_show
+end
+
+crumb :item_edit do
+  link "商品の編集", edit_item_path(params[:id])
+  parent :item_show
+end
+
+crumb :item_new do
+  link "商品の出品", new_item_path
+end
+
+# crumb :projects do
+#   link "Projects", projects_path
+# end
+
+# crumb :project do |project|
+#   link project.name, project_path(project)
+#   parent :projects
+# end
+
+# crumb :project_issues do |project|
+#   link "Issues", project_issues_path(project)
+#   parent :project, project
+# end
+
+# crumb :issue do |issue|
+#   link issue.title, issue_path(issue)
+#   parent :project_issues, issue.project
+# end
+
+# If you want to split your breadcrumbs configuration over multiple files, you
+# can create a folder named `config/breadcrumbs` and put your configuration
+# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
+# folder are loaded and reloaded automatically when you change them, just like
+# this file (`config/breadcrumbs.rb`).
