c993b430d528a08eaa3ffe9782accdc457eafe51
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-08-16 13:11:19 +0900

    検索条件保持
    what
    検索条件の保持ができるようにした
    チェックボックス未
    why
    再検索の際１から入力しなおさないといけなかったから

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,8 +7,19 @@ class SearchesController < ApplicationController
   end
 
   def detail_search
+    # @category = Category.find(params[:category_id_eq])
     @search = params[:search]
     @search_item = Item.ransack(params[:q]) 
+    @sorts = params[:q][:sorts]
+    if params[:q][:category_id_eq].present?
+      @grandchild = Category.find(params[:q][:category_id_eq])
+      @child = @grandchild.parent
+      @parent = @child.parent
+    end
+
+    # @category_id_eq = params[:q][:category_id_eq]
+    
+    @condition_in = params[:q][:condition_in]
     @items = @search_item.result.page(params[:page])
   end
 
