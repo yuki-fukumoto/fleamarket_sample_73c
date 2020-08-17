7f791eed3b2a8ab493672285d6aff2068ceaf44a
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-08-17 21:53:06 +0900

    販売済のアイテムを一覧から排除
    what
    販売済のアイテムを検索結果一覧から排除
    不要な記述削除
    why
    販売済のアイテムは表示不要だと思ったから

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,25 +2,22 @@ class SearchesController < ApplicationController
   before_action :set_ransack
 
   def index
-    @items = Item.search(params[:search]).order(created_at: :desc)
+    @items = Item.search(params[:search]).get_on_sell.order(created_at: :desc)
     @search = params[:search]
   end
 
   def detail_search
-    # @category = Category.find(params[:category_id_eq])
     @search = params[:search]
-    @search_item = Item.ransack(params[:q]) 
+    @search_item = Item.ransack(params[:q])
     @sorts = params[:q][:sorts]
     if params[:q][:category_id_eq].present?
       @grandchild = Category.find(params[:q][:category_id_eq])
       @child = @grandchild.parent
       @parent = @child.parent
     end
-
-    # @category_id_eq = params[:q][:category_id_eq]
     
     @condition_in = params[:q][:condition_in]
-    @items = @search_item.result.page(params[:page])
+    @items = @search_item.result.page(params[:page]).get_on_sell 
   end
 
   private
