65c91fa72ad5fed620b076a1bb06082a97c8ecb1
skipman-77 <h.morikawa5201@gmail.com>
2020-08-17 20:00:14 +0900

    user_registration_error_message
    #what
    郵便番号の入力を３ケタ、４ケタに設定
    半角数字のみ入力可に設定
    #why
    見栄えをよくするため

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
-  validates :firstname, :lastname, :firstname_read, :lastname_read, :zip, :prefecture, :city, :address_line, presence: {message: "が空欄です"}
-  validates :zip, length: {is: 7, message: "は７桁で入力してくだい"}
+  validates :firstname, :lastname, :firstname_read, :lastname_read, :prefecture, :city, :address_line, presence: {message: "が空欄です"}
+  validates :zip, format: { with: /\A\d{3}\-?\d{4}\z/,  message: "郵便番号は半角数字７桁で入力してくだい"} 
   validates :firstname, :lastname, :prefecture, :city, format: { with: /\A[一-龥ぁ-ん]/, message: "は全角で入力してください"}
   validates :firstname_read, :lastname_read, format: { with: /\A[ぁ-んー－]+\z/, message: "は全角(ひらがな)で入力してください"}
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :nickname, :firstname, :lastname, :firstname_read, :lastname_read, :birthday, :password, presence: true
   validates :firstname_read, :lastname_read, format: { with: /\A[ぁ-んー－]+\z/, message: "は全角(ひらがな)で入力してください"}
-
   validates :password, length: { minimum:7, message: "は7文字以上で入力してください"}, confirmation: {message: "の値が一致しません"}
   validates :email, presence: {message: "が空欄です"}
 
@@ -9,8 +8,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
 
   has_many :addresses, dependent: :destroy
   accepts_nested_attributes_for :addresses
