f6e9c8740e461edbd5b623b4eb08bddcd67a05a9
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-08-19 12:29:55 +0900

    指摘事項修正
    what
    不要な記述削除
    why
    不要な記述削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   root 'items#index'
 
   resources :items, except: :index do
-    # collection do
-    #   get 'search'
-    # end
   end  
 
   resources :searches do
