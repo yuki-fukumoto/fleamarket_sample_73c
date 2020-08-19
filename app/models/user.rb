c73a3f3cf32780a0c7ee5d18aeae6a2eb58b69c6
skipman-77 <h.morikawa5201@gmail.com>
2020-08-19 20:40:31 +0900

    user_registration_error_message
    #what
    氏名のふりがな入力でふりがなのみ入力できるように実装
    #why
    ふりがな以外も入力できていたため

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :nickname, :firstname_read, :lastname_read, :birthday, :password, presence: true
   validates :firstname, :lastname, format: {with:/[一-龥ぁ-ん]/, message: "漢字で入力してください"}
-  validates :firstname_read, :lastname_read, format: { with: /\A[ぁ-んー－]+\z/, message: "ひらがなで入力してください"}
+  validates :firstname_read, :lastname_read, format: { with:/[ぁ-ん]/, message: "ひらがなで入力してください"}
   validates :password, length: { minimum:7, message: "は7文字以上で入力してください"}, confirmation: {message: "の値が一致しません"}
   validates :email, presence: {message:"が空欄です"}
 
