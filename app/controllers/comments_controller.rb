d470717aa2ec0030ef03d89c340a7b956073954c
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-08-20 20:04:24 +0900

    コメント機能バリデーション（エラーメッセージ表示可能になった）
    ＃What
    未入力、30文字以上で入力したときに表示
    エラーハンドリング実装中
    ＃Why
    Textをコメントに変更できたら
    レビュー

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,26 @@
 class CommentsController < ApplicationController
   def create
+    @item = Item.find(params[:item_id])
+    @comment = Comment.new(comment_params)
     
-    @item = Item.new{"item_comments_path"}
-    # @comment = Comment.create(comment_params)
-    @comment.valid?
-    @comment.errors
-    if @comment.save
-      redirect_to item_comments_path, notice: 'メッセージが送信されました'
+    if @comment.invalid?
+      redirect_to (@item),alert: @comment.errors.full_messages
     else
-      @comments = @item.comments.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :new
+      @comment.save!
+      redirect_to (@item), notice: 'コメントが投稿されました'
+    end
+   
   end
-end
+    # @comment.valid?
+    # if @comment.save
+    #   redirect_to (@item), notice: 'コメントが投稿されました'
+#     else
+#       @comment.errors
+#       @comments = @item.comments.includes(:user)
+#       flash.now[:alert] = 'メッセージを入力してください。'
+#       redirect_to (@item)
+#   end
+# end
 private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :text, presence: true, length: { maximum: 20 }
+  validates :text,presence: true,length: {maximum: 30}
   belongs_to :item
   belongs_to :user
 end
