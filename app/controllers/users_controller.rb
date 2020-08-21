d5708be179a5114e09fade13780a25f8f75025a6
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-08-22 00:00:39 +0900

    出品した商品一覧
    what
    出品した商品一覧表示
    why
    自分の出品した商品を編集や削除する際見つけやすくするため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @items = Item.where(user_id: current_user.id).includes(:images).order('created_at DESC').page(params[:page]).without_count.per(4)
+    @sale_items = @items.get_on_sell
+    # @sale_items = Item.search(params[:search]).get_on_sell.order(created_at: :desc)
+  end
+  
   def destroy
   end
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
-  resources :users, only: :show
+  resources :users, only: [:index, :show]
 
   root 'items#index'
 
