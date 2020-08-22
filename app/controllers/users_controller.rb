65eb80694c39a5c859a0d28fb48cdda26c6774d9
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-08-22 23:41:18 +0900

    不要な記述削除
    what
    不要な記述削除
    why
    コードを見やすくするため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
   
   def destroy
   end
+
   def show
-    @recently_visit = cookies[:item_name].split(',') if cookies[:item_name]
+    @recently_visit = cookies[:item].split(',') if cookies[:item]
     @items = Item.search(params[:search]).get_on_sell.order(created_at: :desc).limit(4)
   end
+  
   def change
   end
 end
