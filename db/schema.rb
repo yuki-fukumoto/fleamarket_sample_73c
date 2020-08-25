f457d3210455fed715ee6da909641034668faf0a
skipman-77 <h.morikawa5201@gmail.com>
2020-08-25 21:20:13 +0900

    user_registration_error_message
    #what
    migrationfileの型変更
    電話番号のvalidationの削除
    #why
    エラーの解消のため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_112448) do
+ActiveRecord::Schema.define(version: 2020_08_25_114524) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "firstname", null: false
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_112448) do
     t.string "firstname_read", null: false
     t.string "lastname_read", null: false
     t.string "zip", null: false
-    t.string "prefecture_id", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address_line", null: false
     t.string "building"
@@ -91,6 +91,12 @@ ActiveRecord::Schema.define(version: 2020_07_30_112448) do
     t.index ["creditcard_id"], name: "index_purchases_on_creditcard_id"
     t.index ["item_id"], name: "index_purchases_on_item_id"
     t.index ["user_id"], name: "index_purchases_on_user_id"
+  end
+
+  create_table "telephones", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "first_telephone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
