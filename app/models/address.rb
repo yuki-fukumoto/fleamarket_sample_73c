a97ef497a4fe2b036857438f8f04a3ccd8610601
skipman-77 <h.morikawa5201@gmail.com>
2020-08-25 10:22:04 +0900

    user_registration_error_message
    #what
    電話番号の最初の空欄２〜４けたまで入力可とする
    #why
    市外局番及び携帯番号を想定したため

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,7 +7,7 @@ class Address < ApplicationRecord
   validates :firstname, :lastname, :city, format: { with: /\A[一-龥ぁ-ん]/, message: "は全角で入力してください"}
   validates :firstname_read, :lastname_read, format: { with:/[ぁ-ん]/, message: "ひらがなで入力してください"}
   validates :prefecture, format: { with:/[ぁ-ん]/, message: "都道府県を選択して下さい"} 
-
+  validates :first_telephone, format:{with: /0\d{2,4}/}
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   attr_accessor :first_zip, :last_zip, :first_telephone, :second_telephone, :third_telephone
