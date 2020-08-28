de4e81084aa0344bbe1fb0df77c80de50eb68f39
skipman-77 <h.morikawa5201@gmail.com>
2020-08-29 06:20:46 +0900

    user_registration_error_message
    #what
    エラーメッセージに「都道府県」追加
    番地に半角数字のみを適用
    #why
    見栄えを良くする為
    vakidadtionがかかっていなかった為

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,13 @@
 class Address < ApplicationRecord
 
 
-  validates :firstname_read, :lastname_read, :prefecture, :city, :address_line, presence: true
+  validates :firstname_read, :lastname_read, :prefecture, :city, presence: true
   validates :zip, format: { with: /\A\d{3}\-?\d{4}\z/,  message: "郵便番号は半角数字７桁で入力してくだい"} 
   validates :firstname, :lastname, format: {with:/[一-龥ぁ-ん]/, message: "名前を入力してください"}
   validates :firstname, :lastname, :city, format: { with: /\A[一-龥ぁ-ん]/, message: "は全角で入力してください"}
   validates :firstname_read, :lastname_read, format: { with:/[ぁ-ん]/, message: "ひらがなで入力してください"}
   validates :prefecture, format: { with: nil, message: "都道府県を選択して下さい"} 
-  # validates :first_telephone, format:{with: /0\d{2,4}/}
+  validates :address_line, format: { with: /[0-9]/,  message: "半角数字で入力してください"}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
