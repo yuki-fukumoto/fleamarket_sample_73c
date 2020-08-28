9c3bbba6e20af3663f1bb2a4eeeac47f89945fb9
skipman-77 <h.morikawa5201@gmail.com>
2020-08-28 22:40:36 +0900

    user_registration_error_message
    #what
    schemaの変更
    #why
    必要な為

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_114524) do
+ActiveRecord::Schema.define(version: 2020_07_30_112448) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "firstname", null: false
@@ -91,12 +91,6 @@ ActiveRecord::Schema.define(version: 2020_08_25_114524) do
     t.index ["creditcard_id"], name: "index_purchases_on_creditcard_id"
     t.index ["item_id"], name: "index_purchases_on_item_id"
     t.index ["user_id"], name: "index_purchases_on_user_id"
-  end
-
-  create_table "telephones", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "first_telephone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
