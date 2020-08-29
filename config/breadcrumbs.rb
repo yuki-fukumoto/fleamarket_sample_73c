c49424fce09f1f79ba2512bd089b09f8db2bfe11
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-08-29 11:25:44 +0900

    パンくず追加
    what
    パンくずナビ機能実装時、存在しなかったページにパンくず追加
    why
    パンくずを全ページに網羅させるため

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -24,8 +24,8 @@ crumb :creditcard_show do
   parent :user_show
 end
 
-crumb :item_search do
-  link "検索結果一覧", search_items_path
+crumb :search_index do
+  link "検索結果一覧", searches_path
 end
 
 crumb :item_show do
@@ -44,6 +44,26 @@ end
 
 crumb :item_new do
   link "商品の出品", new_item_path
+end
+
+crumb :users_bought_items do
+  link "購入した商品", bought_items_users_path
+  parent :user_show
+end
+
+crumb :users_sale_items do
+  link "出品中の商品", sale_items_users_path
+  parent :user_show
+end
+
+crumb :users_sold_items do
+  link "販売済の商品", sold_items_users_path
+  parent :user_show
+end
+
+crumb :browsing_histories do
+  link "閲覧履歴", browsing_histories_path
+  parent :user_show
 end
 
 # crumb :projects do
