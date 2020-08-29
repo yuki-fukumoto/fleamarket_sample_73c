21d888d2c5657439a50adb50f1feb96846ac637f
skipman-77 <h.morikawa5201@gmail.com>
2020-08-29 20:57:00 +0900

    user_registration_error_message
    #what
    都道府県4文字以上はエラーになるように実装
    #why
    漢字4文字以上の都道府県は存在しないため

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,7 +7,8 @@ class Address < ApplicationRecord
   validates :firstname, :lastname, :city, format: { with: /\A[一-龥ぁ-ん]/, message: "は全角で入力してください"}
   validates :firstname_read, :lastname_read, format: { with:/[ぁ-ん]/, message: "ひらがなで入力してください"}
   validates :prefecture, format: { with: /[一-龥]/,  message: "を入力して下さい"} 
-  validates :prefecture, format: { with: /(北海道|^青森|^岩手|^宮城|^秋田|^山形|^福島|^茨城|^栃木|^群馬|^埼玉|^千葉|^東京|^神奈川|^新潟|^富山|^石川|^福井|^山梨|^長野|^岐阜|^静岡|^愛知|^三重|^滋賀|^京都府|^大阪|^兵庫|^奈良|^和歌山|^鳥取|^島根|^岡山|^広島|^山口|^徳島|^香川|^愛媛|^高知|^福岡|^佐賀|^長崎|^熊本|^大分|^宮崎|^鹿児島|^沖縄)/}
+  validates :prefecture, format: { with: /(北海道|^青森|^岩手|^宮城|^秋田|^山形|^福島|^茨城|^栃木|^群馬|^埼玉|^千葉|^東京|^神奈川|^新潟|^富山|^石川|^福井|^山梨|^長野|^岐阜|^静岡|^愛知|^三重|^滋賀|^京都|^大阪|^兵庫|^奈良|^和歌山|^鳥取|^島根|^岡山|^広島|^山口|^徳島|^香川|^愛媛|^高知|^福岡|^佐賀|^長崎|^熊本|^大分|^宮崎|^鹿児島|^沖縄)/}
+  validates :prefecture, length: { maximum:4}
   validates :address_line, format: { with: /[0-9]/,  message: "半角数字で入力してください"}
 
   # extend ActiveHash::Associations::ActiveRecordExtensions
