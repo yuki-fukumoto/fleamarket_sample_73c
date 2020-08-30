ee30039b0ef2ae669400d5817485bf3ddc21d65b
skipman-77 <h.morikawa5201@gmail.com>
2020-08-30 18:39:19 +0900

    fix_user_registration_error_message
    #what
    refactoring branchからfix branchを作成し、セレクトボックスに該当するcommitを削除
    #why
    masterにmergeした時にerrorにならないように

--- db/migrate/20200829102444_rename_prefecture_id_column_to_addresses.rb
+++
@@ -1,5 +0,0 @@
-class RenamePrefectureIdColumnToAddresses < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :addresses, :prefecture_id, :prefecture
-  end
-end
--- db/migrate/20200829102919_change_datatype_addresses_of_prefecture.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeAddressesOfPrefecture < ActiveRecord::Migration[5.2]
-  def change
-    change_column :addresses, :prefecture, :string
-  end
-end
